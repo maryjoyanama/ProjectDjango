e80d19877a9fbfed3c9a764684c8dd3dbce53eaf
Oding <droiddelmonte@gmail.com>
2019-01-07 00:33:26 +0800

    Refactor and add new filter by database name

--- momoo/src/momoo/routers.py
+++ momoo/src/momoo/routers.py
@@ -1,5 +1,5 @@
 from rest_framework import routers
-from posts.viewsets import PostsViewSet, PostsStatusSuccessViewSet, PostsStatusFailedViewSet, BadgeStatusSuccessViewSet, BadgeStatusFailedViewSet
+from posts.viewsets import PostsViewSet, PostsStatusSuccessViewSet, PostsStatusFailedViewSet, BadgeStatusSuccessViewSet, BadgeStatusFailedViewSet, DatabaseNamesViewSet
 
 router = routers.DefaultRouter()
 router.register(r'posts', PostsViewSet)
@@ -7,3 +7,4 @@ router.register(r'statussuccess', PostsStatusSuccessViewSet)
 router.register(r'statusfailed', PostsStatusFailedViewSet)
 router.register(r'badgestatussuccess', BadgeStatusSuccessViewSet)
 router.register(r'badgestatusfailed', BadgeStatusFailedViewSet)
+router.register(r'databasenames', DatabaseNamesViewSet)
